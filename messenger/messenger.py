f32a43a2ba9f019e6ee681cb2bdf84d02ad4a72b
Audrey Salmon <asalmon@unc.edu>
2025-10-26 17:49:11 -0400

    first method attempted

--- messenger/messenger.py
+++ messenger/messenger.py
@@ -1,6 +1,9 @@
 import os
 import pickle
 import string
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 class MessengerServer:
     def __init__(self, server_signing_key, server_decryption_key):
@@ -25,8 +28,12 @@ class MessengerClient:
         self.certs = {}
 
     def generateCertificate(self):
-        raise Exception("not implemented!")
-        return
+        self.own_dh_keypair =  ec.generate_private_key(ec.SECP384R1())
+        certificate = {
+            'name': self.name,
+            'pk': self.own_dh_keypair.public_key()
+        }
+        return pickle.dumps(certificate)
 
     def receiveCertificate(self, certificate, signature):
         raise Exception("not implemented!")
